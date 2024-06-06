5b678b62497d7c8079be1343debf0f1dee47bc26
girl-codes <168541859+girl-codes@users.noreply.github.com>
2024-06-06 14:27:07 +0100

    Changed temperature

--- src/Weather.js
+++ src/Weather.js
@@ -4,7 +4,7 @@ import "./Weather.css";
 export default function Weather() {
   let weatherData = {
     city: "London",
-    temperature: 19,
+    temperature: 29,
     date: "Tuesday 10:00",
     description: "Cloudy",
     imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
